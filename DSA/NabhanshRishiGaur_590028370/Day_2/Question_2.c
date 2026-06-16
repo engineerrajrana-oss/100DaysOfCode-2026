251c3f46fcb1715fadfad501812e908f3b3ee8f7
Nabhansh <146766007+Nabhansh@users.noreply.github.com>
2026-06-16 07:50:03 +0530

    Update Question_2.c

--- DSA/NabhanshRishiGaur_590028370/Day_2/Question_2.c
+++ DSA/NabhanshRishiGaur_590028370/Day_2/Question_2.c
@@ -4,16 +4,15 @@ int findLargest(int arr[], int n) {
     int largest = arr[0];
 
     for (int i = 1; i < n; i++) {
-        if (arr[i] > largest) {
+        if (arr[i] > largest)
             largest = arr[i];
-        }
     }
 
     return largest;
 }
 
 int main() {
-    int arr[] = {3, 7, 9, 2, 5};
+    int arr[] = {12, 35, 1, 10, 34, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     printf("%d\n", findLargest(arr, n));
